a6d4adad7de05ec225e98c2b102d78d82b7752d5
Davide Faconti <davide.faconti@gmail.com>
2022-10-13 11:15:47 +0200

    fix link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,17 +51,15 @@ const config = {
             },
           },
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/BehaviorTree/btcpp_website/tree/master',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/BehaviorTree/btcpp_website/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
