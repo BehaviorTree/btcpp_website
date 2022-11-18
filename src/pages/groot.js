c3978a281a736c082a912ea7a6a8ed77318e8f17
jaydip-begerz <jaydipz@begerz.com>
2022-11-18 18:44:17 +0530

    groot page ui improvements - manage space between first section and header - reduce font size - put box shadow to the image - improve Groot card design - improve table design, change icon of check and cross and added tooltip in ‘Actively developed’ column - create download button and improve its design

--- src/pages/groot.js
+++ src/pages/groot.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Link from '@docusaurus/Link';
+
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './groot.module.css';
 import Table from 'react-bootstrap/Table';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 function Check(props) {
-  return <img src={useBaseUrl('img/green_check.svg')} width="15" alt="yes" />;
+  return <img src={useBaseUrl('img/check.png')} width="15" alt="yes" />;
 }
 
 function Cross(props) {
-  return <img src={useBaseUrl('img/red_cross.svg')} width="13" alt="no" />;
+  return <img src={useBaseUrl('img/cross.png')} width="15" alt="no" />;
 }
 
 export default function Groot() {
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" className={styles.toolTip} {...props}>
+      some extra information
+    </Tooltip>
+  );
   return (
     <Layout title="Groot" description="Groot Editor">
-      <div className={styles.sectionText}>
-        <div className="container padding-vert--xl text--left {styles.sectionText}">
+
+      {/* groot intro */}
+      <div className={`styles.sectionText`}>
+        <div className={`container text--left ${styles.sectionText}`}>
           <div className="row">
             <div className="col col--6">
               <h1>Groot</h1>
@@ -31,8 +40,8 @@ export default function Groot() {
                   <li>Debug and test your behaviors (<b>new in version 2</b>)</li>
                 </ul>
               </p>
-              <p>
                 <h3>Groot 2 is under development, available in early preview</h3>
+              <p>
                 We are reimaginging Groot to be easier to use and more scalable, focusing in particular
                 on the needs of industrial and professional users.
                 <br />If you use BT.CPP and Groot professionally, <a href="mailto:dfaconti@aurynrobotics.com">contact us</a> and
@@ -41,6 +50,7 @@ export default function Groot() {
             </div>
             <div className="col col--6">
               <img
+              className={styles.grootImg}
                 src={useBaseUrl('img/groot_1.png')}
                 alt="groot 1" />
             </div>
@@ -48,39 +58,39 @@ export default function Groot() {
         </div>
       </div>
 
+      {/* groot versions & cards */}
       <div className={styles.sectionText}>
-        <h1>Which version of Groot is the right for you?</h1>
       </div>
-
-      <div className={styles.sectionText}>
-        <div className="container text--left {styles.sectionText}">
-          <div className="row align-items-center">
-            <div className="col col--5 col--offset-1">
-              <h2 ><img
+      <div className={`${styles.sectionText} ${styles.flexCol}`}>
+        <h1>Which version of Groot is the right for you?</h1>
+        <div className={`container  text--left ${styles.sectionText}`}>
+          <div className={`row ${styles.grootCardWrapper}`}>
+            <div className={styles.grootCard}>
+              <h2><img
                 src={useBaseUrl('img/pro_logo_icon.svg')}
                 alt="pro" width="40"/>   Groot 2.X</h2>
               
               <p><b>Groot 2</b> is the professional successor of Groot, focusing on improved
                 user experience, modularity and scalability.
-                <br />It introduces powerful debugging and introspection tools, to support the
+                It introduces powerful debugging and introspection tools, to support the
                 entire development cycle, from conception to validation.
               </p>
             </div>
-            <div className="col col--5  ">
-            <h2 ><img
+            <div className={styles.grootCard}>
+            <h2><img
                 src={useBaseUrl('img/open_source_icon.svg')}
                 alt="pro" width="40"/>   Groot 1.0</h2>
               <p><b>Groot</b> is the original <a href="https://github.com/BehaviorTree/Groot">open source editor</a>.
-                <br />It is not under active development, but it still a valid
+                It is not under active development, but it still a valid
                 option for users who are still using <b>BehaviorTree.CPP 3.8</b> and need
                 real-time visualization.</p>
             </div>
           </div>
         </div>
       </div>
-
-      <div className={styles.sectionText}>
-        <Table >
+      {/* groot features table */}
+      <div className={`container ${styles.sectionText} `}>
+        <Table className={styles.grootFeatureTable}>
           <thead>
             <tr>
               <th scope="col"></th>
@@ -95,7 +105,16 @@ export default function Groot() {
               <td><Cross /></td>
             </tr>
             <tr>
-              <th scope="row">Actively developed</th>
+              <th scope="row">
+                Actively developed
+                <OverlayTrigger
+                  placement="bottom"
+                  delay={{  hide: 400 }}
+                  overlay={renderTooltip}
+                >
+                  <button className={styles.tooltipTrigger}>?</button>
+                </OverlayTrigger>
+              </th>
               <td><Cross /></td>
               <td><Check /></td>
             </tr>
@@ -140,11 +159,42 @@ export default function Groot() {
         </Table>
       </div>
 
-      <div className={styles.sectionText}>
+     {/* groot download  */}
+      <div className={`${styles.sectionText} container ${styles.flexCol} gap-5 `}>
+      <h1>Download Groot 2.0</h1>
       <p>
-        <h1>Download Groot 2.0</h1>
         <b>Free</b> as in "beer".   <b>Editor Mode</b> only.   <b>Version:</b> alpha_1.
       </p>
+      <div className={styles.downloadGroup}>
+        <div>
+          <img className={styles.downloadLogo} src={useBaseUrl('img/linux.png')} alt="linux logo"/>
+          <Link
+            className="button button--outline button--primary"
+            to={useBaseUrl('/')}
+          >
+            Download for linux
+          </Link>
+        </div>
+        <div>
+          <img className={styles.downloadLogo} src={useBaseUrl('img/windows.png')} alt="windows logo"/>
+          <Link
+            className="button button--outline button--primary"
+            to={useBaseUrl('/')}
+          >
+            Download for windows
+          </Link>
+        </div>
+        <div>
+          <img className={styles.downloadLogo} src={useBaseUrl('img/mac.png')} alt="apple logo"/>
+          <Link
+            className="button button--outline button--primary"
+            to={useBaseUrl('/')}
+          >
+            Download for mac
+          </Link>
+        </div>
+
+      </div>
       </div>
 
     </Layout>
