b50e31479d6e4d5ef030ee5b63edc4b12a563621
Davide Faconti <davide.faconti@gmail.com>
2022-11-24 19:12:55 +0100

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,9 +39,8 @@ const config = {
       /** @type {import('@dipakparmar/docusaurus-plugin-umami').Options} */
       ({
         websiteID: "b0133a07-1c59-49aa-8f9c-7a0c25711718", // Required
-        analyticsDomain: "analytics.behaviortree.dev", // Required
+        analyticsDomain: "https://behaviortree-cpp-umami.netlify.app", // Required
         scriptName: "umami.js",
-        dataHostURL: "https://behaviortree-cpp-umami.netlify.app",
         dataAutoTrack: true, // Optional
         dataCache: true, // Optional
         dataDomains: "behaviortree.dev", // comma separated list of domains, *Recommended*
